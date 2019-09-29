d355eb3d698ccadd8df710eb6f04c660fbc40e78
root <root@vps261977.vps.ovh.ca>
2019-09-29 17:29:05 -0400

    add app.js add package.json

---
+++ app.js
@@ -0,0 +1,41 @@
+const http = require("http");
+const https = require("https");
+let playerCount = 0;
+let serverCount = 0;
+
+setInterval(function() {
+  https.get('https://servers-live.fivem.net/api/servers/', (resp) => {
+    resp.setEncoding('utf8');
+    let data = '';
+
+    // A chunk of data has been recieved.
+    resp.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    // The whole response has been received. Print out the result.
+    resp.on('end', () => {
+      playerCount = 0;
+      serverCount = 0;
+      let newData = JSON.parse(data)
+      newData.forEach((item, i) => {
+        playerCount = playerCount + item.Data.clients;
+        serverCount++;
+      });
+      console.log(playerCount, serverCount)
+    });
+
+  }).on("error", (err) => {
+    console.log("Error: " + err.message);
+  });
+}, 60000)
+
+
+
+http.createServer(function(request, response){
+
+    // response.setHeader("UserId", 12);
+    // response.setHeader("Content-Type", "text/html; charset=utf-8;");
+    response.write(JSON.stringify({playerCount : playerCount, serverCount : serverCount}));
+    response.end();
+}).listen(3000);
